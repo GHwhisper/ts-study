cbd8983aaa90497b227951198fcc834045836306
liangshuang <lsbright@qq.com>
2022-06-25 23:30:09 +0800

    setup细节问题代码整理

--- 02_Vue3/vue3_study/src/App.vue
+++ 02_Vue3/vue3_study/src/App.vue
@@ -1,33 +1,12 @@
 <template>
-  <h2>App 父级组件</h2>
-  <h3>msg: {{ msg }}</h3>
-  <button @click="msg += '==='">更新数据</button>
-  <hr>
-  <child :msg="msg" msg2="真香" @xxx="xxx"></child>
+  <div></div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-// 引入子级组件Child
-import Child from './components/Child.vue'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'App',
-  // 注册组件
-  components: {
-    Child,
-  },
-  setup() {
-    // 定义一个 Ref 类型的数据
-    const msg = ref('what are you doing')
-    function xxx(txt: string) {
-      msg.value += txt
-    }
-    return {
-      msg,
-      xxx
-    }
-  }
 })
 </script>
 
