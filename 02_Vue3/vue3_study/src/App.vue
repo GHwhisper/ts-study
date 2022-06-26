e1037e57a2681d72fb9de55de0d20fba50555139
liangshuang <lsbright@qq.com>
2022-06-26 17:24:40 +0800

    自定义hook函数-鼠标位置

--- 02_Vue3/vue3_study/src/App.vue
+++ 02_Vue3/vue3_study/src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <h2>App 父级组件</h2>
-  <button @click="isShow = !isShow">切换显示</button>
-  <hr>
-  <child v-if="isShow"></child>
+  <h2>自定义 hook 函数操作</h2>
+  <h2>x:{{ x }}, y:{{ y }}</h2>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import Child from './components/Child.vue'
+import { defineComponent } from 'vue'
+import useMousePosition from './hooks/useMousePosition'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    Child,
-  },
+  // 需求1：用于再页面中点击页面，把点击的位置的横纵坐标收集起来并展示出来
   setup() {
-    const isShow = ref(true)
+    const { x, y } = useMousePosition()
+
     return {
-      isShow
+      x,
+      y
     }
   }
 })
