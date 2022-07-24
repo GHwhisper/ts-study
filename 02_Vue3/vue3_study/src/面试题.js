baae84fe1e770661c0e1f65f647fef1c3cd984fb
liangshuang <lsbright@qq.com>
2022-07-24 17:40:30 +0800

    面试题

---
+++ 02_Vue3/vue3_study/src/面试题.js
@@ -0,0 +1,15 @@
+/**
+ * Vue2 和 Vue3 相比较的相关面试题
+ *
+ * 2020年9月发布的正式版
+ * Vue3 支持大多数的 Vue2 的特性
+ * Vue 中设计了一套强大的 Composition API（组合API） 代替了 Vue2 中的 Option API（选项API）
+ * 更好地支持 TS
+ * 最主要：Vue3 中使用了 Proxy 配合 Reflect 代替了 Vue2 中的 object.defineProperty() 方法实现数据的响应式（数据代理）
+ * 重写了虚拟 DOM，速度更快了
+ * 新的组件：Fragment（片段） / Teleport（瞬移） / Suspense（不确定）
+ * 设计了一个新的脚手架工具：Vite
+ *
+ * ......
+ *
+ */
